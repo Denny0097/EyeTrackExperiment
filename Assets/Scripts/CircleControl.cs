692c3ea8e0de1df37eb6d6268e9e5546d882cbd9
Denny0097 <144431036+Denny0097@users.noreply.github.com>
2023-10-13 18:07:19 +0800

    del event& merge

--- Assets/Scripts/CircleControl.cs
+++ Assets/Scripts/CircleControl.cs
@@ -8,6 +8,7 @@ using UnityEngine.UI;
 
 public class CircleControl : MonoBehaviour
 {
+    public GameObject intro;
     public GameObject Crosshair;
     public GameObject circle_center;
     public GameObject circle_right;
@@ -26,7 +27,9 @@ public class CircleControl : MonoBehaviour
     
     private void Start()
     {
-        /*Crosshair = GameObject.Find("Crosshair");
+        /*  I dont know how to set
+        intro = Gamebject.Find("InstroctionCanvas");
+        Crosshair = GameObject.Find("Crosshair");
         circle_center = GameObject.Find("circle_center");
         circle_right = GameObject.Find("circle_right");
         circle_left = GameObject.Find("circle_left");
@@ -43,10 +46,11 @@ public class CircleControl : MonoBehaviour
 
         //set init material
         //circle_center.GetComponent<Renderer>().material = material1;
-        if (circle_center != null)
+
+        /*if (circle_center != null)
         {
             circle_center.GetComponent<Renderer>().material = material1;
-        }
+        }*/
         StartCoroutine(ShowAndHideUI());
         
 
@@ -54,97 +58,104 @@ public class CircleControl : MonoBehaviour
 
     private IEnumerator ShowAndHideUI()
     {
-
-        StreamWriter streamWriter = File.CreateText(RoundSetPath);
-        while (count <= 188) 
+        //AnyKey to start
+        if (Input.anyKeyDown)
         {
-            //output message: round detail
-            RoundSet = $"round {count}:\n";
+            //turn off intro view
+            intro.SetActive(false);
 
-            //first 8 round are traning
-            if (count == 8)
+            StreamWriter streamWriter = File.CreateText(RoundSetPath);
+            while (count <= 188)
             {
-                //message for traning finished 
-                StartWords.SetActive(true);
-                yield return new WaitForSeconds(10f);
-                StartWords.SetActive(false);
-                yield return new WaitForSeconds(2.0f);
+                //output message: round detail
+                RoundSet = $"round {count}:\n";
 
-            }
-            //color choose
-            if (Random.Range(0, 2) == 0)
-            {
-                circle_center.GetComponent<Renderer>().material = material1;
-                RoundSet += "color :purple ,";
-            }
-            else
-            {
-                circle_center.GetComponent<Renderer>().material = material2;
-                RoundSet += "color :brown ,";
-            }
-            //crosshair
-            Crosshair.SetActive(true);
-            yield return new WaitForSeconds(1.0f);
+                //first 8 round are traning
+                if (count == 8)
+                {
+                    //message for traning finished 
+                    StartWords.SetActive(true);
+                    yield return new WaitForSeconds(10f);
+                    StartWords.SetActive(false);
+                    yield return new WaitForSeconds(2.0f);
 
-            Crosshair.SetActive(false);
-            yield return new WaitForSeconds(0.2f);
-
-            //center dot
-            circle_center.SetActive(true);
-            yield return new WaitForSeconds(1.0f);
-
-            circle_center.SetActive(false);
-            yield return new WaitForSeconds(0.2f);
-
-            //////////////////
-            //!! something is wrong !!
-            //////////////////
-            //R or L dot :position choose
-            if (Random.Range(0, 2) == 0)
-            {
-                Debug.Log("Displaying rightObject");
-                circle_right.SetActive(true);
-                RoundSet += "position :right\n";
-
-                //output to console
-                Debug.Log(RoundSet);
-                
-                if (circle_center.GetComponent<Renderer>().material == material1)
-                    Debug.Log("should RIGHT\n");
+                }
+                //color choose
+                if (Random.Range(0, 2) == 0)
+                {
+                    circle_center.GetComponent<Renderer>().material = material1;
+                    RoundSet += "color :purple ,";
+                }
                 else
-                    Debug.Log("should LEFT\n");
-            }
-            else
-            {
-                Debug.Log("Displaying leftObject");
-                circle_left.SetActive(true);
-                RoundSet += "position :left\n";
+                {
+                    circle_center.GetComponent<Renderer>().material = material2;
+                    RoundSet += "color :brown ,";
+                }
 
-                //output to console
-                Debug.Log(RoundSet);
+                //crosshair
+                Crosshair.SetActive(true);
+                yield return new WaitForSeconds(1.0f);
 
-                if (circle_center.GetComponent<Renderer>().material == material1)
-                    Debug.Log("should LEFT\n");
+                Crosshair.SetActive(false);
+                yield return new WaitForSeconds(0.2f);
+
+                //center dot
+                circle_center.SetActive(true);
+                yield return new WaitForSeconds(1.0f);
+
+                circle_center.SetActive(false);
+                yield return new WaitForSeconds(0.2f);
+
+                //////////////////
+                //!! something is wrong !!
+                //////////////////
+                //R or L dot :position choose
+                if (Random.Range(0, 2) == 0)
+                {
+                    Debug.Log("Displaying rightObject");
+                    circle_right.SetActive(true);
+                    RoundSet += "position :right\n";
+
+                    //output to console
+                    Debug.Log(RoundSet);
+
+                    if (circle_center.GetComponent<Renderer>().material == material1)
+                        Debug.Log("should RIGHT\n");
+                    else
+                        Debug.Log("should LEFT\n");
+                }
                 else
-                    Debug.Log("should RIGHT\n");
+                {
+                    Debug.Log("Displaying leftObject");
+                    circle_left.SetActive(true);
+                    RoundSet += "position :left\n";
+
+                    //output to console
+                    Debug.Log(RoundSet);
+
+                    if (circle_center.GetComponent<Renderer>().material == material1)
+                        Debug.Log("should LEFT\n");
+                    else
+                        Debug.Log("should RIGHT\n");
+                }
+
+                //message save
+                streamWriter.WriteLine(RoundSet);
+
+
+
+                yield return new WaitForSeconds(1.0f);
+
+                circle_right.SetActive(false);
+                circle_left.SetActive(false);
+
+                yield return new WaitForSeconds(1.0f);
+
+                //next round
+                count++;
+
             }
-
-            //message save
-            streamWriter.WriteLine(RoundSet);
-
-            
-
-            yield return new WaitForSeconds(1.0f);
-
-            circle_right.SetActive(false);
-            circle_left.SetActive(false);
-
-            yield return new WaitForSeconds(1.0f);
-
-            //next round
-            count++;
-
+            streamWriter.Close();
         }
-        streamWriter.Close();
     }
 }
